bff7c58104bde70af350cd081236ec03d1101654
Ignas Stanionis <Ignas_Stanionis@epam.com>
2021-09-08 16:28:08 +0300

    prettier

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -1,55 +1,51 @@
-import path from "path";
-import {Configuration} from "webpack";
-import ESLintPlugin from "eslint-webpack-plugin";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from 'path';
+import { Configuration } from 'webpack';
+import ESLintPlugin from 'eslint-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 const config: Configuration = {
-    mode: "development",
-    entry: "./index.tsx",
-    output: {
-        publicPath: ""
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/i,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "@babel/preset-env",
-                            "@babel/preset-react",
-                            "@babel/preset-typescript",
-                        ],
-                    },
-                },
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html"
-        }),
-        new ForkTsCheckerWebpackPlugin({
-            async: false
-        }),
-        new ESLintPlugin({
-            extensions: ["js", "jsx", "ts", "tsx", "json", "html", "css"],
-        }),
+  mode: 'development',
+  entry: './index.tsx',
+  output: {
+    publicPath: '',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
     ],
-    devtool: "inline-source-map",
-    devServer: {
-        static: path.join(__dirname, "build"),
-        historyApiFallback: true,
-        port: 8080,
-        open: true,
-        hot: true
-    },
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'html', 'css'],
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.join(__dirname, 'build'),
+    historyApiFallback: true,
+    port: 8080,
+    open: true,
+    hot: true,
+  },
 };
 
 export default config;
