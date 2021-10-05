a4133c903365dbd45a5de49ba8a1eccfb0911e81
Ignas Stanionis <Ignas_Stanionis@epam.com>
2021-10-05 11:42:52 +0300

    dots

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dots from 'assets/icons/dots.svg';
 
 interface MovieCardProps {
   imageSource: string;
@@ -7,17 +8,29 @@ interface MovieCardProps {
   description: string;
 }
 
-const MovieCard = ({ imageSource, title, years, description }: MovieCardProps) => (
-  <div className="flex-col">
-    <div className="w-auto pb-4">
-      <img src={imageSource} />
+const MovieCard = ({ imageSource, title, years, description }: MovieCardProps) => {
+  const [isBlurred, setIsBlurred] = useState(false);
+
+  const onCardMouseEnter = () => setIsBlurred(true);
+  const onCardMouseLeave = () => setIsBlurred(false);
+
+  return (
+    <div className="flex-col cursor-pointer" onMouseEnter={onCardMouseEnter} onMouseLeave={onCardMouseLeave}>
+      <div className="relative pb-4">
+        {isBlurred && (
+          <div className="absolute top-2 right-2">
+            <Dots />
+          </div>
+        )}
+        <img src={imageSource} />
+      </div>
+      <p className="flex w-full">
+        <span>{title}</span>
+        <span className="ml-auto px-2 py-1 text-xs border-2 rounded-lg border-gray-500">{years}</span>
+      </p>
+      <small>{description}</small>
     </div>
-    <p className="flex w-full">
-      <span>{title}</span>
-      <span className="ml-auto px-2 py-1 text-xs border-2 rounded-lg border-gray-500">{years}</span>
-    </p>
-    <small>{description}</small>
-  </div>
-);
+  );
+};
 
 export default MovieCard;
