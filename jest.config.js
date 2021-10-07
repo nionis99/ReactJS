4b13c6c9b043ddf42798bed08c12390159c58d30
Ignas Stanionis <Ignas_Stanionis@epam.com>
2021-10-07 13:58:37 +0300

    fixed tests

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules', 'src'],
   transform: {
     '\\.tsx?$': 'ts-jest',
     '\\.jsx?$': 'babel-jest',
   },
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/file-mock.js',
+    '.+\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|svg|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
   },
   testPathIgnorePatterns: [`node_modules`, `\\.cache`],
 };
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App from 'App';
+import App from './App';
 
 test('check first div text', () => {
   const { getByText } = render(<App />);
-  expect(getByText('Welcome')).toBeInTheDocument();
+  expect(getByText('FIND YOUR MOVIE')).toBeInTheDocument();
 });
