646af8cab5547d725e9b1fd63169154c323bc466
Ignas Stanionis <Ignas_Stanionis@epam.com>
2021-10-08 14:37:40 +0300

    added additional options

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Dots from 'assets/icons/dots.svg';
+import XIcon from 'assets/icons/x.svg';
 
 interface MovieCardProps {
   imageSource: string;
@@ -10,9 +11,15 @@ interface MovieCardProps {
 
 const MovieCard = ({ imageSource, title, years, description }: MovieCardProps) => {
   const [isBlurred, setIsBlurred] = useState(false);
+  const [hasMoreActionSelected, setHasMoreActionSelected] = useState(false);
 
+  const onMoreActionClicked = () => setHasMoreActionSelected(true);
+  const onMoreActionClose = () => setHasMoreActionSelected(false);
   const onCardMouseEnter = () => setIsBlurred(true);
-  const onCardMouseLeave = () => setIsBlurred(false);
+  const onCardMouseLeave = () => {
+    if (hasMoreActionSelected) setHasMoreActionSelected(false);
+    setIsBlurred(false);
+  };
 
   return (
     <div
@@ -22,9 +29,20 @@ const MovieCard = ({ imageSource, title, years, description }: MovieCardProps) =
     >
       <div className="relative pb-4">
         {isBlurred && (
-          <div className="absolute top-2 right-2">
-            <Dots />
-          </div>
+          <>
+            <div className="absolute top-2 right-2" onClick={onMoreActionClicked}>
+              <Dots />
+            </div>
+            {hasMoreActionSelected && (
+              <ul className="absolute w-48 right-2 py-1 mt-2 bg-white bg-content rounded shadow-modal">
+                <p className="w-full">
+                  <XIcon className="ml-auto mb-1 mr-2 w-3 h-3" onClick={onMoreActionClose} />
+                </p>
+                <li className="px-4 py-2 text-white hover:bg-primary">Edit</li>
+                <li className="px-4 py-2 text-white hover:bg-primary">Delete</li>
+              </ul>
+            )}
+          </>
         )}
         <img src={imageSource} alt={title} />
       </div>
